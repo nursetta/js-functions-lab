4a6618a5a89772d265805a06e3bd9a8e2c6d0e15
nursetta <n_ursetta@yahoo.com>
2016-06-15 22:56:19 -0600

    6 of 8 solutions

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,29 +1,56 @@
 // Question 1
-function maxOfTwoNumbers() {
-
-}
+function maxOfTwoNumbers(num1, num2) {
+	if (num1 > num2) {
+		return num1;
+	} else {
+		return num2;
+}}
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(num1, num2, num3) {
+	if (num1 > num2 && num1 > num3) {
+		return num1;
+	} 
+	else if (num1 < num2 && num2 > num3) {
+		return num2;
+	}
+	else {
+		return num3;
+	}	
 }
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(char) {
+	if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // Question 4
-function sumArray() {
- 
+var arrayOfNumbers = [1,2,3,4];
+
+function sumArray(num) {
+	var sum = 0;
+	for (var i = 0; i < num.length; i++) {
+		sum += num[i];
+}
+	return sum;	
 }
 
 
 // Question 4
-function multiplyArray() {
+var arrayOfNumbers = [1,2,3,4];
 
+	function sumArray(num) {
+		var multiply = 1;
+		for (var i = 0; i < num.length; i++) {
+			multiply *= num[i];
+	}
+		return multiply;
+	
 }
-
 
 // Question 5
 var numberOfArguments = function(){
@@ -33,14 +60,24 @@ var numberOfArguments = function(){
 
 
 // Question 6
-var reverseString = function (){
-  
-};
+var reverseString = function(string) {
+  var rev = '';
+  for (var i = string.length - 1; i >= 0; i--)
+    rev += string[i];
+  return rev;
+}
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord(str) {
+  var split = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < split.length; i++){
+    if(split[i].length > longestWord){
+	longestWord = split[i].length;
+     }
+  }
+  return longestWord;
 }
 
 
